2ae5f794cf0a8e76699ed31d175b0c9a8a75a92f
MunseeLogan-FS <lmmunsee@student.fullsail.edu>
2025-05-15 19:18:11 -0400

    uploading assignment

--- app/index.js
+++ app/index.js
@@ -1,27 +1,55 @@
 const express = require("express");
 const morgan = require("morgan");
-const routeHandler = require("../app/routes/");
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
 
-// localhost:3000
-app.get("/", (req, res) => {
-  res.status(200).json({
-    message: "This is the API",
-    sucess: true,
-  });
-});
+async function getPokemonData(id) {
+  console.log(id);
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    if (!response.ok) {
+      throw new Error("Network response not OK");
+    }
+    const data = await response.json();
+    const name = data.name;
+    const height = data.height;
+    const weight = data.weight;
+    const types = data.types.map((type) => type.type.name);
 
-//localhost:3000/api/v1
-app.use("/api/v1", routeHandler);
+    const getSpecies = await fetch(data.species.url);
+    if (!getSpecies.ok) throw new Error("Failed to fetch species");
+    const speciesData = await getSpecies.json();
 
-app.use("/*wildcard", (req, res) => {
-  res.status(404).json({
-    message: "This is not a valid endpoint",
-    sucess: false,
-  });
-});
+    const englishFlavor = speciesData.flavor_text_entries.find(
+      (entry) => entry.language.name === "en"
+    );
+    const flavorText = englishFlavor.flavor_text.replace(/\f|\n/g, " ");
+    const habitat = speciesData.habitat.name;
+    const legendary = speciesData.is_legendary;
+
+    // console.log(speciesData);
+    return {
+      name,
+      height,
+      weight,
+      types,
+      flavorText,
+      habitat,
+      legendary,
+    };
+  } catch (error) {
+    console.error("Fetch error:", error);
+  }
+}
+
+async function assignmentTask() {
+  const randomID = Math.floor(Math.random() * 151) + 1;
+  const pokemon = await getPokemonData(randomID);
+  console.log(pokemon);
+}
+
+assignmentTask();
 
 module.exports = app;
