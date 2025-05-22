dfbee705b0f070cc2030e268a35426cffbdb66e7
MunseeLogan-FS <lmmunsee@student.fullsail.edu>
2025-05-22 00:26:18 -0400

    started setup

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,19 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const heroRoutes = require("./heroRoutes");
-const villainRoutes = require("./villainRoutes");
 
 router.get("/", (req, res) => {
-  res
-    .status(200)
-    .json({
-      success: true,
-      message: `${req.method} - Request made`,
-      availableRoutes: ["/heroes", "/villains"],
-    });
+  res.status(200).json({
+    success: true,
+    message: `${req.method} - Request made`,
+    availableRoutes: ["/heroes", "/villains"],
+  });
 });
 
 router.use("/heroes", heroRoutes);
-router.use("/villains", villainRoutes);
 
 module.exports = router;
